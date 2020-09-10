676204a8e662a1b4e068c40a662706fd1eb6fbd7
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-09-10 23:53:20 +0300

    HTTP POST Request using fetch api

--- scripts/app-es6.js
+++ scripts/app-es6.js
@@ -5,3 +5,15 @@ const request = new ES6HTTP();
 request.get('https://jsonplaceholder.typicode.com/users')
   .then(data => console.log('Fetch api: ', data))
   .catch(err => console.log('Fetch api error:', err))
+
+
+// Create new user
+const user = {
+  name: "John Doe",
+  username: "johndoe",
+  email: "johndoe@somedomain.com"
+}
+// Post request
+request.post('https://jsonplaceholder.typicode.com/users', user)
+       .then( data => console.log(data))
+       .catch( err => console.log(err));
--- scripts/es6-http.js
+++ scripts/es6-http.js
@@ -18,4 +18,17 @@
        }
      });
    }
-  }
+
+   // HTTP POST Request
+   post(url, data) {
+     return fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(res => res.json())
+      .catch(err => err)
+   }
+}
